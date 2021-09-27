53a7a4783cafc68fbae84a9dcea3fade5dc89d6a
viniciusoliveirabittencourt <viniciusbittencourt2003@gmail.com>
2021-09-27 17:03:12 -0300

    requisito 2

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,76 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
+import { createUser } from '../services/userAPI';
+import Carregando from './Carregando';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      carregando: false,
+      redirect: false,
+    };
+  }
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  onClick = async () => {
+    const { name } = this.state;
+    this.setState({
+      carregando: true,
+    });
+    await createUser({ name });
+    this.setState({
+      redirect: true,
+    });
+  }
+
+  disbleButton() {
+    const { name } = this.state;
+    const number = 3;
+    return name.length < number;
+  }
+
+  corpoDoLogin(carregando, form, redirect) {
+    if (carregando === true) {
+      if (redirect === true) {
+        return <Redirect to="/search" />;
+      }
+      return <Carregando />;
+    }
+    return form;
+  }
+
   render() {
+    const { name, carregando, redirect } = this.state;
+    const form = (
+      <form>
+        <label htmlFor="name">
+          Nome:
+          <input
+            data-testid="login-name-input"
+            name="name"
+            value={ name }
+            onChange={ this.handleChange }
+          />
+        </label>
+        <button
+          data-testid="login-submit-button"
+          type="button"
+          disabled={ this.disbleButton() }
+          onClick={ this.onClick }
+        >
+          Entrar
+        </button>
+      </form>);
     return (
-      <h1>Login</h1>
+      this.corpoDoLogin(carregando, form, redirect)
     );
   }
 }
